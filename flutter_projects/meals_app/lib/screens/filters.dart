6e952a0641cb0d2cba3d0d105bdb1e4e0d3edd44
Marx_Mas <116768281+LuisReynoso233531@users.noreply.github.com>
2025-12-16 10:50:05 -0700

    Meals App

---
+++ flutter_projects/meals_app/lib/screens/filters.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/screens/tabs.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
+
+enum Filter { glutenFree, lactoseFree, vegetarian, vegan }
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key, required this.currentFilters});
+
+   final Map<Filter,bool>currentFilters;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FilterScreen();
+  }
+}
+
+class _FilterScreen extends State<FiltersScreen> {
+  var _glutenFreeFilterSet = false;
+  var _lactoseFreeFilterSet = false;
+  var _vegetarianFreeFilterSet = false;
+  var _veganFreeFilterSet = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _glutenFreeFilterSet = widget.currentFilters[Filter.glutenFree]!;
+    _lactoseFreeFilterSet = widget.currentFilters[Filter.lactoseFree]!;
+    _veganFreeFilterSet = widget.currentFilters[Filter.vegan]!;
+    _vegetarianFreeFilterSet = widget.currentFilters[Filter.vegetarian]!;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Your filters')),
+      //   drawer: MainDrawer(
+      //     onSelectScreen: (indentifier) {
+      //       Navigator.of(context).pop();
+      //       if (indentifier == 'meals') {
+      //         Navigator.of(
+      //           context,
+      //         ).push(MaterialPageRoute(builder: (ctx) => TabsScreen()));
+      //       }
+      //     },
+      //   ),
+      body: PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (bool didPop, dynamic result) {
+          if (didPop) return;
+          Navigator.of(context).pop({
+            Filter.glutenFree: _glutenFreeFilterSet,
+            Filter.lactoseFree: _lactoseFreeFilterSet,
+            Filter.vegetarian: _vegetarianFreeFilterSet,
+            Filter.vegan: _veganFreeFilterSet,
+          });
+        },
+
+        child: Column(
+          children: [
+            SwitchListTile(
+              value: _glutenFreeFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _glutenFreeFilterSet = isChecked;
+                });
+              },
+              title: Text(
+                'Gluten-free',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+              subtitle: Text(
+                'Only include gluten-free meals.',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+              activeThumbColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            ),
+            SwitchListTile(
+              value: _lactoseFreeFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _lactoseFreeFilterSet = isChecked;
+                });
+              },
+              title: Text(
+                'Lactose-free',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+              subtitle: Text(
+                'Only include lactose-free meals.',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+              activeThumbColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            ),
+            SwitchListTile(
+              value: _vegetarianFreeFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _vegetarianFreeFilterSet = isChecked;
+                });
+              },
+              title: Text(
+                'Vegetarian-free',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+              subtitle: Text(
+                'Only include vegetarian-free meals.',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+              activeThumbColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            ),
+            SwitchListTile(
+              value: _veganFreeFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _veganFreeFilterSet = isChecked;
+                });
+              },
+              title: Text(
+                'Vegan-free',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+              subtitle: Text(
+                'Only include vegan-free meals.',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+              activeThumbColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_projects/meals_app/lib/screens/meals.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+import 'package:meals_app/screens/meals_detailes.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, this.title, required this.meals, required this.onToggleFavorite});
+
+  final String? title;
+  final List<Meal> meals;
+    final void Function(Meal meal) onToggleFavorite;
+
+  void selectMeal(BuildContext context, Meal meal){
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailScreen(meal: meal, onToggleFavorite: onToggleFavorite,),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Uh oh... Nothing here!',style: Theme.of(context).
+             textTheme.headlineLarge!.copyWith(color:
+              Theme.of(context).colorScheme.onBackground.withOpacity(0.7)
+              ),),
+            const SizedBox(height: 16),
+            Text('Try selecting a different category!',
+             style: Theme.of(context).
+             textTheme.bodyLarge!.copyWith(color:
+              Theme.of(context).colorScheme.onBackground.withOpacity(0.7)
+              ),),
+          ],
+        ),
+      );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (context, index) => MealItem(meal: meals[index], onSelectMeal: (context, meal){
+        selectMeal(context, meal);
+      },),
+    );
+    }
+
+    if(title == null){
+      return content;
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: Text(title!)),
+      body: content
+    );
+  }
+}
